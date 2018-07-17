64437f7fa5cf558b848f686beda3a816d0586278
Dan McKinney <3dhdaniel@gmail.com>
2018-07-17 13:52:40 -0400

    practice

---
+++ counters/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+//  return <h1>Hello</h1>;
+    return (
+    <div className='container'>
+      <button>+</button>
+      <div className='counterBox'>
+        <div className='counter'>3</div>
+        <div className='counter'>1</div>
+        <div className='counter'>1</div>
+      </div>
+    </div>
+    );
+
+};
+
+export default App;
--- udemy/annoy.js
+++
@@ -1,9 +0,0 @@
-
-
-
-
-
-for(var i = 300; i <= 333; i++) {
-	if(i % 2 !== 0);
-	console.log(i);
-}
